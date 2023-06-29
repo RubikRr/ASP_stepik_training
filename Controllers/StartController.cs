f3fb92141c9abd681f135356874c7537c7d85286
Ukropchik <vova.chakalov.2003@gmail.com>
2023-06-29 20:24:25 +0300

    выполнил задачу 1 из пунтка 4.2

---
+++ Controllers/StartController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WomanShop.Controllers
+{
+    public class StartController : Controller
+    {
+        public string Hello()
+        {
+            var hours=DateTime.Now.TimeOfDay.Hours;
+            if (hours >= 0 && hours <= 5)
+            {
+                return "Ночь";
+            }
+            else if (hours >= 6 && hours <= 11)
+            {
+                return "Утро";
+            }
+            else if (hours >= 12 && hours <= 17)
+            {
+                return "День";
+            }
+            else if (hours >= 18 && hours <= 23)
+            {
+                return "Вечер";
+            }
+            else
+            {
+                return "Вы на другой планете";
+            }
+            
+        }
+    }
+}
