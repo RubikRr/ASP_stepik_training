cff54286c06912df4a5955a6cb2f8de4f000c76c
Ukropchik <vova.chakalov.2003@gmail.com>
2023-07-06 20:50:38 +0300

    исправил задание 4 из пункта 10.3

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -4,9 +4,25 @@ namespace WomanShop.Controllers
 {
     public class AdminController:Controller
     {
-        public IActionResult Index(string str)
+        public IActionResult Index()
         {
-            return View("index",str);
+            return View();
+        }
+        public IActionResult Orders()
+        {
+            return View();
+        }
+        public IActionResult Users()
+        {
+            return View();
+        }
+        public IActionResult Roles()
+        {
+            return View();
+        }
+        public IActionResult Products()
+        {
+            return View();
         }
     }
 }
