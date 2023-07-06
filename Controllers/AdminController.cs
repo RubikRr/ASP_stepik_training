c3eb5653d94ed1953f631030d24d254f4f26b059
Ukropchik <vova.chakalov.2003@gmail.com>
2023-07-06 18:58:08 +0300

    выполнил задание 4 из пункта 10.3

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WomanShop.Controllers
+{
+    public class AdminController:Controller
+    {
+        public IActionResult Index(string str)
+        {
+            return View("index",str);
+        }
+    }
+}
